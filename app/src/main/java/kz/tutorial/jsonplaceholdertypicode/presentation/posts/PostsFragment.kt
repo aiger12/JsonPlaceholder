1b8442485f9ada4a51e54ac6a1aa1a6540a52df4
Aigerim S <98973748+aiger12@users.noreply.github.com>
2024-05-23 19:26:01 +0500

    user profile page added

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/posts/PostsFragment.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/posts/PostsFragment.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import kz.tutorial.jsonplaceholdertypicode.R
 import kz.tutorial.jsonplaceholdertypicode.presentation.utils.ClickListener
